ed7ead0e4835c972bac07c40f02a376e27693a90
Marwan Saadeh <marwansaadeh@outlook.com>
2026-06-15 20:48:59 +0200

    Added test project

---
+++ ParkingManagementSystem.Test/GarageTest.cs
@@ -0,0 +1,38 @@
+﻿using ParkingManagementSystem.Entities;
+using ParkingManagementSystem.Utilites;
+
+namespace ParkingManagementSystem.Test
+{
+    public class GarageTest
+    {
+        private readonly Seeder seeder = new Seeder();
+        private readonly Garage<Vehicle> garage;
+        private readonly Vehicle vehicle;
+
+        public GarageTest()
+        {
+            garage = new Garage<Vehicle>(1);
+            vehicle = seeder.vehicles[0];
+        }
+
+        [Fact]
+        public void Park_WhenPassVehicle_Should_ReturnTrue()
+        {
+            bool result = garage.Park(vehicle);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Park_WhenPassVehicleWithNoCapacity_Should_Returnfalse()
+        {
+            var garage = new Garage<Vehicle>(0);
+            var vehicle = seeder.vehicles[0];
+
+            bool result = garage.Park(vehicle);
+
+            Assert.False(result);
+        }
+
+    }
+}
